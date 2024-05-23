e1798e24037f2e7d7ce864b889afd12664b6e638
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-05-23 23:12:05 +0100

    new commit bg

--- home.js
+++ home.js
@@ -52,3 +52,15 @@ window.onscroll = function() {
     prevScrollpos = currentScrollPos;
 }
 
+
+let currentSlide = 0;
+
+function changeSlide(direction) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = slides.children.length;
+    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+    const slideWidth = slides.children[0].clientWidth;
+    slides.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+}
+
+
